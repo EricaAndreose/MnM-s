eec20ef1d9d2cb467f3be2c2a8d7ef34f371fb3f
EriA <83347847+EricaAndreose@users.noreply.github.com>
2023-11-27 21:25:03 +0100

    Add files via upload

---
+++ changeStyle.js
@@ -0,0 +1,19 @@
+document.addEventListener('DOMContentLoaded', function() {
+    const styleButtons = document.querySelectorAll('.style-btn');
+    const styleLink = document.getElementById('style-link');
+    const vinylImage = document.getElementById('rotate');
+
+    styleButtons.forEach(function(button) {
+        button.addEventListener('click', function() {
+            const stylePath = this.getAttribute('data-style');
+            styleLink.href = stylePath;
+        
+            // Check if the button has data-src attribute
+            const newSrc = this.getAttribute('data-src');
+            if (newSrc) {
+                vinylImage.src = newSrc;
+
+            }
+        });
+    });
+});
---
+++ tryajax.js
@@ -0,0 +1,21 @@
+document.addEventListener('DOMContentLoaded', function() {
+  const loadButtons = document.querySelectorAll('.dropdown-item');
+  const contentColumn = document.getElementById('externalContentColumn');
+
+  loadButtons.forEach(function(button) {
+      button.addEventListener('click', function() {
+          const articlePath = this.getAttribute('data-article');
+          
+          // Fetch the content from the specified articlePath
+          fetch(articlePath)
+              .then(response => response.text())
+              .then(content => {
+                  // Insert the fetched content into the contentColumn
+                  contentColumn.innerHTML = content;
+              })
+              .catch(error => {
+                  console.error('Error loading external content:', error);
+              });
+      });
+  });
+});
